32b9621889c49e958918156ee8b935e388bf7649
JOÃO VITOR LUCAS SCHMIDT <joao_vl_schmidt@estudante.sesisenai.org.br>
2024-08-29 10:54:40 -0300

    banco com modal

---
+++ app/(tabs)/FormaTransacao.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+const TransactionForm = ({ valor, setValor, onDeposit, onWithdraw }) => {
+  
+  const handleChangeText = (text) => {
+    const numericValue = text.replace(/[^0-9.]/g, '');
+    setValor(numericValue);
+  };
+
+  return (
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        keyboardType='numeric'
+        value={valor}
+        onChangeText={handleChangeText}
+        placeholder="Digite o valor"
+      />
+      <View style={styles.buttonContainer}>
+        <Button color='red' title="Depositar" onPress={onDeposit} />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button color='red' title="Sacar" onPress={onWithdraw} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  form: {
+    width: '80%',
+    margin: 20,
+  },
+  buttonContainer: {
+    marginBottom: 10,
+    paddingBottom: 5, 
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+  },
+});
+
+export default TransactionForm;
